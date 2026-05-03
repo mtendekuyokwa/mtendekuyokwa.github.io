221d8396691a94b8c9b34eca07bacc118bda18e9
mtende kuyokwa <mtendekuyokwa19@gmail.com>
2026-05-03 21:22:45 +0200

    adding finn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,24 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders } from "astro/config";
+import tailwind from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
+  vite: {
+    plugins: [tailwind()],
+  },
+  site: "https://mtendekuyokwa.github.io",
+  integrations: [mdx(), sitemap()],
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Afacad Flux",
+      cssVariable: "--font-atkinson",
+      fallbacks: ["sans-serif"],
+      options: {},
+    },
+  ],
 });
